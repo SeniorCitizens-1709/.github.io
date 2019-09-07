e7457765cac20bfebcd34262daf62fed63731afc
Your Name <125294303@qq.com>
2019-09-07 16:07:47 +0800

    update

---
+++ src/blog/components/ReactSenior/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+// react高级
+const Gaoji = [
+  {
+    route: "Top-Api"
+  },
+  {
+    route: "Top-Api"
+  },
+  {
+    route: "Top-Api"
+  },
+  {
+    route: "Top-Api"
+  },
+  {
+    route: "Top-Api"
+  },
+  {
+    route: "Top-Api"
+  }
+];
+export default class ReactSenior extends Component {
+  render() {
+    return <div>高级</div>;
+  }
+}
--- src/blog/index.jsx
+++ src/blog/index.jsx
@@ -1,42 +1,42 @@
-import React, { Component } from 'react'
-import Mylayout from './components/layout';
+import React, { Component } from "react";
+import Mylayout from "./components/layout";
 // 引入React基础概述
-import ReactBase from './components/ReactBase';
-import 'antd/dist/antd.css';
+import ReactBase from "./components/ReactBase";
+import "antd/dist/antd.css";
 // react组件分类
-import ComponentType from './components/componentType';
+import ComponentType from "./components/componentType";
+import ReactSenior from "./components/ReactSenior";
 
 export default class index extends Component {
-	constructor(){
-		super()
-		this.state = {
-			isCom:window.location.hash.slice(1) || 'ReactBase'
-		}
-		
-		window.onhashchange = ()=>{
-			this.setState({
-				isCom:window.location.hash.slice(1)
-			})
-		}
-	};
-	// 收听子组件传递数据
-	getSonData = (id)=>{
-		window.location.hash = id;
-	}
-	// 渲染函数
-	render() {
-		let Dom = '';
-		switch(this.state.isCom){
-			case 'ReactBase':
-				Dom = <ReactBase />;
-				break;
-			case 'ComponentType':
-				Dom = <ComponentType />
-		}
-		return (
-			<Mylayout getsondata={this.getSonData}>
-				{Dom}
-			</Mylayout>
-		)
-	}
+  constructor() {
+    super();
+    this.state = {
+      isCom: window.location.hash.slice(1) || "ReactBase"
+    };
+
+    window.onhashchange = () => {
+      this.setState({
+        isCom: window.location.hash.slice(1)
+      });
+    };
+  }
+  // 收听子组件传递数据
+  getSonData = id => {
+    window.location.hash = id;
+  };
+  // 渲染函数
+  render() {
+    let Dom = "";
+    switch (this.state.isCom) {
+      case "ReactBase":
+        Dom = <ReactBase />;
+        break;
+      case "ComponentType":
+        Dom = <ComponentType />;
+        break;
+      case "ReactSenior":
+        Dom = <ReactSenior />;
+    }
+    return <Mylayout getsondata={this.getSonData}>{Dom}</Mylayout>;
+  }
 }
