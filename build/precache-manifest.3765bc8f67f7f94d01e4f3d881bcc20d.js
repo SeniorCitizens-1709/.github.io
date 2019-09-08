1218ba05d9f63752a0b3be3c18c70e52f9dbeaec
p-geweirong <15831910734@163.com>
2019-09-08 18:47:26 +0800

    打包上线

---
+++ build/precache-manifest.3765bc8f67f7f94d01e4f3d881bcc20d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0248e7a39a5a128d1dffc235b4f9fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53853c69656260bc8113",
+    "url": "./static/css/2.ca38c8f4.chunk.css"
+  },
+  {
+    "revision": "99628bd698899e33ecfb",
+    "url": "./static/css/main.dc511630.chunk.css"
+  },
+  {
+    "revision": "53853c69656260bc8113",
+    "url": "./static/js/2.2d8bee28.chunk.js"
+  },
+  {
+    "revision": "99628bd698899e33ecfb",
+    "url": "./static/js/main.70f2d2f6.chunk.js"
+  },
+  {
+    "revision": "d0ca5e2e399527d265ae",
+    "url": "./static/js/runtime~main.c80c8cbd.js"
+  },
+  {
+    "revision": "de79985e1e46dbebc07f9456ee382d17",
+    "url": "./static/media/bs-004-fu.de79985e.png"
+  },
+  {
+    "revision": "f1edb0eefac41b6fd949860cc7b37f2c",
+    "url": "./static/media/bs-004-zi.f1edb0ee.png"
+  }
+]);
